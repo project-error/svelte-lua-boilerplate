64b166d539b084409e9f9c85588e10b7831f573c
Luke <sabolukas03@gmail.com>
2021-12-18 15:19:08 +0100

    chore: Param intellisense comments

--- web/src/utils/fetchNui.ts
+++ web/src/utils/fetchNui.ts
@@ -1,3 +1,10 @@
+/**
+* @param eventName - The endpoint eventname to target
+* @param data - Data you wish to send in the NUI Callback
+*
+* @return returnData - A promise for the data sent back by the NuiCallbacks CB argument
+*/
+
 export async function fetchNui<T = any>(
   eventName: string,
   data: unknown = {}
--- web/src/utils/useNuiEvent.ts
+++ web/src/utils/useNuiEvent.ts
@@ -5,6 +5,18 @@ interface NuiMessage<T = unknown> {
   data: T;
 }
 
+/**
+ * A function that manage events listeners for receiving data from the client scripts
+ * @param action The specific `action` that should be listened for.
+ * @param handler The callback function that will handle data relayed by this function
+ *
+ * @example
+ * useNuiEvent<{visibility: true, wasVisible: 'something'}>('setVisible', (data) => {
+ *   // whatever logic you want
+ * })
+ *
+ **/
+
 export function useNuiEvent<T = unknown>(
   action: string,
   handler: (data: T) => void
